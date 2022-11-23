80538654ede4a285cd54cabdec7b5ed6a499aeb1
Arjun Jadhav <arjun.jadhav@magicedtech.com>
2022-11-23 15:16:15 +0530

    completed with header part

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Header from "./components/Header";
+import "./styles/App.scss";
+import "./styles/header.scss";
+
+function App() {
+  return (
+    <div className="App">
+   <Router>
+    <Header />
+      <Routes>
+        <Route />
+      </Routes>
+   </Router>
+    </div>
+  );
+}
+
+export default App;
